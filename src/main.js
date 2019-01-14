71eddb3d7216a0c5d4bea9212832271e6f340308
Tahsin <41298152+tahsin95@users.noreply.github.com>
2019-01-14 18:20:01 +0700

    Brand New Boilerplate

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/store';
 
 Vue.config.productionTip = false;
+Vue.use(VueResource);
+
+export default {
+  data: {
+    url: {
+      root: 'http://localhost:3000/',
+      // postsUrl: 'http://localhost:3000/posts',
+    },
+  },
+};
 
 new Vue({
   router,
